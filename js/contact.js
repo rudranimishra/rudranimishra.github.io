a847be2772743d8f3fb09417f037693acbe18945
shivanirmishra <shivani007mishra@gmail.com>
2020-06-27 14:48:00 +0530

    update

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,16 @@
 function contactform(){
     $('#contact-form').submit(function(e){
-        var firstname=document.getElementById('inputFirstName'),
-            lastname=document.getElementById('inputLastName'),
-            email=document.getElementById('inputEmail'),
-            phone=document.getElementById('inputPhone'),
-            msg=document.getElementById('inputMessage');
+        var Name=document.getElementById('inputFirstName'),
+            Surname=document.getElementById('inputLastName'),
+            Email=document.getElementById('inputEmail'),
+            Phone_no=document.getElementById('inputPhone'),
+            Message=document.getElementById('inputMessage');
 
         if(!firstname.value || !lastname.value || !email.value || !phone.value || !msg.value){
             alertify.error('please check your entries')
         }else{
             $.ajax({
-                url:"https://formspree.io/shivanirmishra007@gmail.com",
+                url:"https://formspree.io/rudrani.mishra2@gmail.com",
                 method:"POST",
                 data:$(this).serialize(),
                 dataType:"json"
